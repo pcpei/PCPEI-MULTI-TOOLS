d04f93720713832949ea88c36ab129382cfa74de
PCPEI <75542514+coco8900@users.noreply.github.com>
2025-02-04 20:39:10 +0100

    Add files via upload

---
+++ pip install a ouvrir avant.cmd
@@ -0,0 +1,15 @@
+pip install os
+pip install shutil
+pip install requests
+pip install tempfile
+pip install subprocess
+pip install ctypes
+pip install sys
+pip install re
+pip install stat
+pip install webbrowser
+pip install colorama
+pip install speedtest
+pip install psutil
+pip install time
+python PCPEI.py
